82a6ce9adf7e084f3f69fc6da142dc2d14ab9482
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-09-20 22:51:51 -0600

    reset script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
 
-# apt-get update -y
-# apt-get dist-upgrade -y
-# cd /
+apt-get update -y
+apt-get dist-upgrade -y
 tar xzf ./point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
-apt-get install ./point.deb
+apt-get install ./point.deb -y
 point --version
 echo "Hello Pointers!"
