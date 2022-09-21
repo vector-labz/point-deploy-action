454df90d62727e2597d7c8ee931f5fb54c3dc54d
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-09-20 22:32:09 -0600

    comment out upgrade step

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 
 apt-get update -y
-sleep 10
-apt-get dist-upgrade -y
-sleep 10
-tar xzf /point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
-apt-get install /point.deb
+# apt-get dist-upgrade -y
+tar xzf ./point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
+apt-get install ./point.deb
 point --version
 echo "Hello Pointers!"
