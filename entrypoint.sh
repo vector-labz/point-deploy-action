c96b712cdd90aa757e25239e7291943a2ec27fe4
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-09-20 22:39:14 -0600

    comment out all updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-apt-get update -y
+# apt-get update -y
 # apt-get dist-upgrade -y
-cd /
+# cd /
 tar xzf ./point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
 apt-get install ./point.deb
 point --version
