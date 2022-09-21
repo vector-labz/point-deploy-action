98c7e6d2d6e543399058793c7fc5201d0d32e6d9
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-09-20 22:24:47 -0600

    remove dist-upgrade -> upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh -l
 
 apt-get update -y
-
-apt-get dist-upgrade -y
-
+apt-get upgrade -y
 tar xzf point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
-
 ls -la
-
 apt-get install ./point.deb
-
 point --version
-
 echo "Hello Pointers!"
