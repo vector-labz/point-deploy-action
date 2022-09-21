b4c4a875107b6aad7b1a2988bda441c74cc6fd5a
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-09-20 22:29:50 -0600

    add sleeps, change install path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
 apt-get update -y
-apt-get upgrade -y
-tar xzf point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
-ls -la
-apt-get install ./point.deb
+sleep 10
+apt-get dist-upgrade -y
+sleep 10
+tar xzf /point-v0.2.72-Linux-Debian-Ubuntu.tar.gz
+apt-get install /point.deb
 point --version
 echo "Hello Pointers!"
